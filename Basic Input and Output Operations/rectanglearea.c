1701db1b7183b0cb6ad36b98bd530bfa885974e9
Aman Singh <138305220+amanchauhan1999@users.noreply.github.com>
2024-12-13 11:23:17 +0530

    Create rectanglearea.c
    Write a program to calculate Area of a rectangle

---
+++ Basic Input and Output Operations/rectanglearea.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int len,br;
+    printf("Enter the length and breadth :");
+    scanf(" %d %d",&len,&br);
+
+    printf("%d",len*br);
+    return 0;
+}
